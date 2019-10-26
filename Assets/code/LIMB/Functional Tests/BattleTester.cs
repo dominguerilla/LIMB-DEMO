5efcd1cad04364cdbfda00d2e9ec5d128e333bcb
Carlos Dominguez <dominguerilla@gmail.com>
2019-10-25 21:06:43 -0400

    Resource init

---
+++ Assets/code/LIMB/Functional Tests/BattleTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LIMB;
+public class BattleTester : MonoBehaviour {
+
+    public bool DebugMode;
+
+    [SerializeField]
+    NPCParty leftParty;
+    [SerializeField]
+    NPCParty rightParty;
+
+    BattleManager bManager;
+
+    private void Awake() {
+        bManager = GameObject.FindObjectOfType<BattleManager>();    
+    }
+
+    public void StartBattle(){
+        bManager.StartBattle(leftParty, rightParty);
+    }
+
+    public void EndBattle(){
+        bManager.EndBattle();
+    }
+}
