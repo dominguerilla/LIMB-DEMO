71f98e5167c64ef3050435ae0c884dc20e26b008
Carlos Dominguez <dominguerilla@gmail.com>
2019-11-02 09:55:55 -0400

    Remove Start and Update functions

--- Assets/code/ActionBuilderUINotifier.cs
+++ Assets/code/ActionBuilderUINotifier.cs
@@ -9,18 +9,6 @@ using Bolt;
 /// </summary>
 public class ActionBuilderUINotifier : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnSkillSelected() {
         CustomEvent.Trigger(this.gameObject, "On Skill Selected");
     }
--- Assets/code/BattleEventNotifier.cs
+++ Assets/code/BattleEventNotifier.cs
@@ -9,18 +9,6 @@ using Bolt;
 /// </summary>
 public class BattleEventNotifier : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void BattleStart() {
         CustomEvent.Trigger(this.gameObject, "Battle Start");
     }
