8e4cf5e1cdfbd2201a74074b1bff2eecd23d3bd9
cdom <dominguerilla@gmail.com>
2020-02-14 16:13:23 -0500

    Add slime attack anim

---
+++ Assets/AnimationTester.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTester : MonoBehaviour
+{
+    public GameObject model;
+    public string animationToTrigger;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = model.GetComponent<Animator>();
+        if (!animator)
+        {
+            Debug.LogError("No Animator component found in " + model.gameObject);
+            Destroy(this);
+        }        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            animator.Play(animationToTrigger);
+        }
+    }
+}
