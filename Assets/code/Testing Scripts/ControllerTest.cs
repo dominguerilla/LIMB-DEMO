14c0f77c37232b6153867d21ea868712c2dc7632
cdom <dominguerilla@gmail.com>
2020-02-18 11:25:36 -0500

    Add script for basic controller test

---
+++ Assets/code/Testing Scripts/ControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3))
+        {
+            Debug.Log("ATK MENU");
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            Debug.Log("RUN AWAY MENU");
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            Debug.Log("MAG MENU");
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            Debug.Log("ITEM MENU");
+        }
+    }
+}
